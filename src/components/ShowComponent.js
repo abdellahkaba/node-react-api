95a0066ee9bd53a16bbff986dc27a6145b300ddf
Abdellah <abdallahkaba98@gmail.com>
2024-04-04 14:49:25 +0000

    gestPost-table

--- src/components/ShowComponent.js
+++ src/components/ShowComponent.js
@@ -15,6 +15,34 @@ export default function ShowComponent() {
     return <>
         <div className='App'>
             <h1>La liste des Post</h1>
+            {
+                posts.data != undefined && posts.data.data.length > 0 && (
+                    <table style={{width:'100%'}} border='1'>
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Date</th>
+                                <th>Image</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                posts.data.data.map(
+                                    post => (
+                                        <tr key={post._id}>
+                                            <td>{post.title}</td>
+                                            <td>{post.date}</td>
+                                            <td>
+                                                <img src={'http://localhost:8000/api/images/'+post.image} style={{width: '100px',height:'100px'}} />
+                                            </td>
+                                        </tr>
+                                    )
+                                )
+                            }
+                        </tbody>
+                    </table>
+                )
+            }
         </div>
     </>
 }

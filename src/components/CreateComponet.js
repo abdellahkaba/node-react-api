b9822593e6aca210ae126e329ac3c4dd12f11b34
Abdellah <abdallahkaba98@gmail.com>
2024-04-04 13:43:47 +0000

    formData-service-for-backend

--- src/components/CreateComponet.js
+++ src/components/CreateComponet.js
@@ -1,10 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import postService from '../services/postService.js'
+
 
 export default function CreateComponet() {
 
-    
+    const [title,setTitle] = useState('')
+    const [date,setDate] = useState('')
+    const [image,setImage] = useState()
+    const [message,setMessage] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+       const formData = new FormData()
+       formData.append('title',title)
+       formData.append('date',date)
+       formData.append('image',image)
+
+       const response = await postService.create(formData)
+      console.log(response)
+
+         
+        
+       e.target.reset()
+       
+    }
 
   return <>
-    <h1>Hello</h1>
+        <div>
+            <h2>Create Post</h2>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type='text' 
+                    value={title}
+                    placeholder='Entrer le Post Title'
+                    onChange={(e) => setTitle(e.target.value)}
+                    required 
+                />
+                <br></br>
+                <input 
+                    type='date' 
+                    value={date}
+                    placeholder='Entrer la date'
+                    onChange={(e) => setDate(e.target.value)}
+                    required 
+                />
+                <br></br>
+                <input 
+                    type='file' 
+                   // value={image}
+                    placeholder='Choisir une image'
+                    onChange={(e) => setImage(e.target.files[0])}
+                    required 
+                /> 
+                <br></br>
+                <button>Submit</button>
+            </form>
+
+            <p>{message}</p>
+        </div>
   </>
 }
---
+++ src/services/postService.js
@@ -0,0 +1,17 @@
+import axios from "axios"
+
+class Post {
+
+    create(formData){
+        const url = "http://localhost:8000/api/create-post"
+
+        const config = {
+            headers : {
+                'Content-type' : 'multipart/form-data'
+            }
+        }
+        return axios.post(url,formData,config)
+    }
+}
+
+export default new Post()
